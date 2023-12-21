3d307f7a11439f279facbc55db1cc43ca766d41a
Davi Maia <Davirtmaia@gmail.com>
2023-12-21 23:00:08 +0100

    Classes : Pieces, board and position created

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,7 +1,14 @@
 package Application;
 
+import BoardGame.Board;
+import BoardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Position pos = new Position(3,6);
+        System.out.println(pos);
+
+        Board board = new Board(8,8);
     }
 }
